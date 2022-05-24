e5a3af55a1be12aa570d6ffce0a41fe504e783b2
Danil Nesterenko <nochzato@gmail.com>
2022-05-25 01:13:53 +0300

    rewrote all functions to @tailrec and implemented reverse method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,13 @@ import scala.annotation.tailrec
 enum MyList[+A]:
   case MyNil
   case MyCons(hd: A, tl: MyList[A])
+  def reverse: MyList[A] =
+    @tailrec
+    def go[A](xs: MyList[A], result: MyList[A] = MyNil): MyList[A] =
+      xs match
+        case MyNil          => result
+        case MyCons(hd, tl) => go(tl, MyCons(hd, result))
+    go(this)
 
 import MyList.*
 
@@ -10,36 +17,41 @@ object MyList {
     xs.foldRight(MyNil: MyList[A]) { case (x, acc) => MyCons(x, acc) }
 }
 
-// TODO rewrite to tailrec
 def takeFirst[A](xs: MyList[A], n: Int): MyList[A] =
-  xs match
-    case MyNil => MyNil
-    case MyCons(hd, tl) =>
-      if n > 0 then MyCons(hd, takeFirst(tl, n - 1)) else MyNil
+  @tailrec
+  def go[A](xs: MyList[A], n: Int, result: MyList[A] = MyNil): MyList[A] =
+    xs match
+      case MyNil => result.reverse
+      case MyCons(hd, tl) =>
+        if n > 0 then go(tl, n - 1, MyCons(hd, result)) else result.reverse
+  go(xs, n)
 
 def takeWhile[A](xs: MyList[A], p: A => Boolean): MyList[A] =
-  xs match
-    case MyNil => MyNil
-    case MyCons(hd, tl) =>
-      if (p(hd)) then MyCons(hd, takeWhile(tl, p)) else MyNil
-
-def unfold[A](a: A)(next: A => Option[A]): MyList[A] =
-  next(a) match
-    case None    => MyNil
-    case Some(i) => MyCons(a, unfold(i)(next))
+  @tailrec
+  def go[A](
+      xs: MyList[A],
+      p: A => Boolean,
+      result: MyList[A] = MyNil
+  ): MyList[A] =
+    xs match
+      case MyNil => result.reverse
+      case MyCons(hd, tl) =>
+        if p(hd) then go(tl, p, MyCons(hd, result)) else result.reverse
+  go(xs, p)
 
 def indexOf[A](xs: MyList[A], a: A): Option[Int] =
   @tailrec
-  def go[A](xs: MyList[A], a: A, index: Int): Option[Int] =
+  def go[A](xs: MyList[A], a: A, index: Int = 0): Option[Int] =
     xs match
       case MyNil => None
       case MyCons(hd, tl) =>
         if hd == a then Some(index)
         else go(tl, a, index + 1)
-  go(xs, a, 0)
+  go(xs, a)
 
 def indexOfNth[A](xs: MyList[A], a: A, position: Int): Option[Int] =
-  def go[A](xs: MyList[A], a: A, position: Int, index: Int): Option[Int] =
+  @tailrec
+  def go[A](xs: MyList[A], a: A, position: Int, index: Int = 0): Option[Int] =
     xs match
       case MyNil => None
       case MyCons(hd, tl) =>
@@ -48,7 +60,15 @@ def indexOfNth[A](xs: MyList[A], a: A, position: Int): Option[Int] =
             case 1 => Some(index)
             case _ => go(tl, a, position - 1, index + 1)
         else go(tl, a, position, index + 1)
-  go(xs, a, position, 0)
+  go(xs, a, position)
+
+def unfold[A](a: A)(next: A => Option[A]): MyList[A] =
+  @tailrec
+  def go[A](a: A, result: MyList[A])(next: A => Option[A]): MyList[A] =
+    next(a) match
+      case None    => result.reverse
+      case Some(i) => go(i, MyCons(a, result))(next)
+  go(a, MyNil)(next)
 
 @main def main: Unit =
   println(
